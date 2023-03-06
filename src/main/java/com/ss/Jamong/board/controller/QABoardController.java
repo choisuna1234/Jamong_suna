7f414ad4b8adc5a79d3b18d02b9d9aaac41308bb
suna <choisuna123@naver.com>
2023-03-06 16:12:08 +0900

    0306

--- src/main/java/com/ss/Jamong/board/controller/QABoardController.java
+++ src/main/java/com/ss/Jamong/board/controller/QABoardController.java
@@ -5,6 +5,7 @@ import com.ss.Jamong.board.service.QABoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,8 +33,10 @@ public class QABoardController {
         System.out.println(qaBoard.getContent());
         return "redirect:/board/list";
     }
+
+    //리스트, 페이지 검색 및 페이징
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size = 5) Pageable pageable,
+    public String list(Model model, @PageableDefault(size = 5, sort = "idx", direction = Sort.Direction.DESC) Pageable pageable,
                        @RequestParam(required = false, defaultValue = "") String searchText) {
         Page<QABoard> boards = qaBoardService.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
         int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
@@ -76,11 +79,8 @@ public class QABoardController {
         qaBoardService.write(qaBoard); //자꾸 boardTemp에 안들어가길래 한번 더 적어줬더니 수정이 됨
         qaBoardService.write(boardTemp); //작성된 제목과 내용을 write에 덮어씌워줌
 
-        System.out.println("--------------" + boardTemp);
         return "redirect:/board/list";
     }
-
-    //게시판 페이징
 
 
 }
